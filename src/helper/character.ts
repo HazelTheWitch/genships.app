2cdaa5655c75d3e56e25a4bdd06fce11534f22e2
Hazel Rella <hazelrella11@gmail.com>
2023-01-27 12:18:19 -0800

    a couple minor things

--- src/helper/character.ts
+++ src/helper/character.ts
@@ -1,34 +1,9 @@
-import genshindb, { type Character } from "genshin-db";
-import type { CharacterInfo } from "@/helper/types";
-
-export const ELEMENT_COLORS: { [name: string]: string } = {
-    Anemo: '#80FFD7',
-    Cryo: '#99FFFF',
-    Dendro: '#B2EA2A',
-    Electro: '#FFACFF',
-    Geo: '#FFE699',
-    Hydro: '#80C0FF',
-    Pyro: '#FF9999',
-}
-
-export function to_character_info(character: Character): CharacterInfo {
-    return {
-        name: character.name,
-        fullname: character.fullname,
-        title: character.title,
-        icon: character.images.icon,
-        cover: character.images.cover1,
-        element_color: (character.element in ELEMENT_COLORS) ? ELEMENT_COLORS[character.element] : "#DBE890",
-        child: character.body == "LOLI",
-    }
-}
-
-export function get_character_info(name: string): CharacterInfo | undefined {
-    const character = genshindb.characters(name, { queryLanguages: [genshindb.Language.English] });
-
-    if (character === undefined) {
-        return undefined;
-    }
-
-    return to_character_info(character);
+export type CharacterInfo = {
+    name: string,
+    fullname: string,
+    title: string,
+    icon: string,
+    cover?: string,
+    element_color: string,
+    child: boolean,
 }
